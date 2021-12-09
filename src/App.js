3da62584f2e3a0c7a3735bf0cbb7104e3c66a972
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-12-08 18:01:39 -0800

    got list of flags to show on screen and tests passing

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { getCountries } from './services/countries';
 function App() {
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [continents, setContinents] = useState('all');
-  const [query, setQuery] = useState('');
+  // const [continents, setContinents] = useState('all');
+  // const [query, setQuery] = useState('');
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCountries();
@@ -20,6 +20,15 @@ function App() {
     <div className="App">
       <h1>flags of the world</h1>
       <input placeholder="country"></input>
+      {countries.map((country) => (
+        <div key={country.id}>
+          <img
+            src={`https://flagcdn.com/16x12/${country.iso2.toLowerCase()}.png`}
+            alt={'Flag of ' + country.name}
+          />
+          <span>{' ' + country.name}</span>
+        </div>
+      ))}
     </div>
   );
 }
